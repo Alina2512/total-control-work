6518d1a4c3489e27722dd8f2464a4e19330b4294
Alina Markovich <alina-46@mail.ru>
2023-02-14 15:49:06 +0300

    third commit

--- Program.cs
+++ Program.cs
@@ -12,3 +12,15 @@ void ShowArray(string[] array)
         Console.Write(array[i] + ", ");
     Console.Write(array[array.Length - 1] + "]\n ");
 }
+void ShowArrayShort(string[] array)
+{
+    Console.Write("[");
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length<=3)
+        {
+            Console.Write(array[i] + ", ");
+        }
+    }
+    Console.Write("]\n ");
+}
