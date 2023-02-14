596fd8f238b8ad7ca81dbe7f3fb25ad80a7a8a57
Alina Markovich <alina-46@mail.ru>
2023-02-14 15:46:07 +0300

    second commit

--- Program.cs
+++ Program.cs
@@ -5,3 +5,10 @@
         array[i] = Console.ReadLine();
     return array;    
 }
+void ShowArray(string[] array)
+{
+    Console.Write("[");
+    for(int i = 0; i < (array.Length - 1); i++)
+        Console.Write(array[i] + ", ");
+    Console.Write(array[array.Length - 1] + "]\n ");
+}
